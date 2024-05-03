83daa4458c0da780a949c072328120e026dce136
Kirill Kizin <kizin1999@mail.ru>
2024-05-03 23:29:42 +0300

    feat: add db scripts

--- db/init_scripts/init_db.sql
+++ db/init_scripts/init_db.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS test_table
     (
-        id serial PRIMARY KEY,
-        num integer,
-        data varchar
+        user_id integer PRIMARY KEY,
+        user_name varchar
     );
