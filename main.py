ecf5beeb360bd92e083fa548514b93278f6bc167
Kirill Kizin <kizin1999@mail.ru>
2024-05-03 23:32:44 +0300

    feat: add FSM

--- main.py
+++ main.py
@@ -1,58 +1,84 @@
 import asyncio
 import logging
 import sys
-
-from aiogram import Bot, Dispatcher, html
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message
-
 from bot_secrets.token import TOKEN
 
-# All handlers should be attached to the Router (or Dispatcher)
-dp = Dispatcher()
+from aiogram import Bot, Dispatcher, Router, html
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.types import (
+    KeyboardButton,
+    Message,
+    ReplyKeyboardRemove,
+    ReplyKeyboardMarkup
+)
+
+import db
+
+example_db = db.DataBase()
+form_router = Router()
 
 
-@dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    """Handle messages with `/start` command."""
-    # Most event objects have aliases for API methods that can be
-    # called in events' context
-    # For example if you want to answer to incoming message
-    # you can use `message.answer(...)` alias
-    # and the target chat will be passed to
-    # :ref:`aiogram.methods.send_message.SendMessage`
-    # method automatically or call API method directly via
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
-    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+class Form(StatesGroup):
+    """States of FSM."""
+
+    greetings = State()
+    getName = State()
+    menu = State()
 
 
-@dp.message()
-async def echo_handler(message: Message) -> None:
-    """
-    Forward receive a message back to the sender.
-
-    By default, message handler will handle all message types
-    (like a text, photo, sticker etc.)
-    """
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
+@form_router.message(CommandStart())
+async def command_start(message: Message, state: FSMContext) -> None:
+    """Run when you type /start."""
+    if not example_db.user_exists(message.from_user.id):
+        await state.set_state(Form.greetings)
+        await process_name(message, state)
+    else:
+        await state.set_state(Form.menu)
+        await menu(message, state)
 
 
-async def main() -> None:
-    """Run main loop."""
-    # Initialize Bot instance with default bot properties
-    # which will be passed to all API calls
-    bot = Bot(
-        token=TOKEN,
-        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+@form_router.message(Form.greetings)
+async def process_name(message: Message, state: FSMContext) -> None:
+    """Get a name."""
+    await message.answer(
+        "Hi there! What's your name?",
+        reply_markup=ReplyKeyboardRemove(),
     )
-    # And the run events dispatching
+    await state.set_state(Form.getName)
+
+
+@form_router.message(Form.getName)
+async def saving_name(message: Message, state: FSMContext) -> None:
+    """Save name to DB."""
+    example_db.add_user(message.from_user.id, message.text)
+
+    await state.set_state(Form.menu)
+    await message.answer(
+        f"Nice to meet you, {html.quote(message.text)}!\n",
+    )
+    await menu(message, state)
+
+
+@form_router.message(Form.menu)
+async def menu(message: Message, state: FSMContext) -> None:
+    """Show menu_keyboard."""
+    kb = [
+        [KeyboardButton(text="Посмотреть текущие привычки")],
+        [KeyboardButton(text="Добавить привычку")],
+        [KeyboardButton(text="Удалить привычку")]
+    ]
+    keyboard = ReplyKeyboardMarkup(keyboard=kb)
+    await message.answer("Выберите действие", reply_markup=keyboard)
+
+
+async def main():
+    """Configure bot."""
+    bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
+    dp = Dispatcher()
+    dp.include_router(form_router)
     await dp.start_polling(bot)
 
 
